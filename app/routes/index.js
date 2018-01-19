ad8c48e06e27803c669c798b1cb8757ed6f72128
Goran Jovanovic <goranj@vast.com>
2018-01-19 19:03:01 +0100

    Socket.io

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,15 +3,16 @@
 const path = require('path');
 
 module.exports = {
-  getHome: (req, res) => {
+  getHome: (req, res, next) => {
     // res.send('home');
     // res.sendFile(__dirname + '../public/index.html');
     res.sendFile('index.html', { root: path.join(__dirname, '../public') });
   },
-  getMap: (req, res) => {
+  getMap: (req, res, next) => {
     res.send('map');
   },
-  getPostData: (req, res) => {
-    res.send('Hello');
+  getPostData: (req, res, next) => {
+    var data = JSON.stringify(req.body);
+    res.send(data);
   },
 };
